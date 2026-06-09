770231bdbb7df229862f210fc5934bf0f704e518
amanverma07-07 <vermaamann007@gmail.com>
2026-06-09 22:38:28 +0530

    Create find_all_the_anagram_in_string.cpp

---
+++ WEEK2/DAY2/find_all_the_anagram_in_string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+
+        if (p.size() > s.size()) return ans;
+
+        vector<int> pCount(26, 0);
+        vector<int> window(26, 0);
+
+        for (char c : p)
+            pCount[c - 'a']++;
+
+        int k = p.size();
+
+        for (int i = 0; i < k; i++)
+            window[s[i] - 'a']++;
+
+        if (window == pCount)
+            ans.push_back(0);
+
+        for (int i = k; i < s.size(); i++) {
+            window[s[i] - 'a']++;       
+            window[s[i - k] - 'a']--;   
+
+            if (window == pCount)
+                ans.push_back(i - k + 1);
+        }
+
+        return ans;
+    }
+};
